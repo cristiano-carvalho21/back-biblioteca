54e27ed96e3853d1b35c588d7f2f607cc1874f71
Cristiano Carvalho <cristianocarvalh207@gmail.com>
2025-10-25 11:34:08 +0100

    Retificando um erro na função cadastro

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ export const handleRegister = async(req, res) => {
         }
         const [usuario, dominio] = email.split('@');
 
-        if(/\.com/i.test(usuario){
+        if(/\.com/i.test(usuario)){
             return res.status(400).json({erro:'Email com o nome mal informado'});
         }
         if(!/\d/.test(usuario)){
